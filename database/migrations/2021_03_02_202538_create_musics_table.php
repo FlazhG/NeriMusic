f89a75e55cea60ffae302669d33d62f804ab6b91
Zhamus <al221810713@gmail.com>
2021-02-17 15:23:24 -0600

    migracion de music

---
+++ database/migrations/2021_03_02_202538_create_musics_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMusicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('musics', function (Blueprint $table) {
+            $table->id('id_music',5);
+            $table->string('nombre_music',30);
+            $table->unsignedBigInteger('id_artis');
+            $table->foreign('id_artis')->references('id_artis')->on('artists');
+            $table->binary('caratula_music');
+            $table->time('duracion_music');
+            $table->unsignedBigInteger('id_genero');
+            $table->foreign('id_genero')->references('id_genero')->on('generos');
+            $table->string('formato_music',3);
+            $table->string ('discografica_music',30);
+            $table->longText('descripcion_music',50);
+            $table->date('fecha_music');
+            $table->unsignedBigInteger('id_album');
+            $table->foreign('id_album')->references('id_album')->on('albums');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('musics');
+    }
+}
