2a5faa4ea85181d599b5b11259bafbe09cea35f8
De La Cruz Rivera Jose Daniel <al221811690@gmail.com>
2021-02-17 16:37:00 -0600

    migracion de artista

--- database/migrations/2021_02_16_234445_create_artists_table.php
+++ database/migrations/2021_02_16_234445_create_artists_table.php
@@ -15,7 +15,18 @@ class CreateArtistsTable extends Migration
     {
         Schema::create('artists', function (Blueprint $table) {
             $table->id('id_artis', 5);
-            
+            $table->string('nombre_artis', 25);
+            $table->string('apellido_artis',25);
+            $table->string('email_artis',30);
+            $table->string('email_verified',35)->unique();
+            $table->date('fecha_artis');
+            $table->enum('sexo_artis',['Masculino','Femenino']);
+            $table->string('password_artis',25);
+            $table->binary('img_artis');
+            $table->string('telefono_artis',10);
+            $table->string('terminos_artis',1);
+            $table->string('disquera_artis',25);
+            $table->string('descripcion_artis',50);
             $table->unsignedBigInteger('rol_id');
             $table->foreign('rol_id')->references('rol_id')->on('rols');
             $table->rememberToken();
