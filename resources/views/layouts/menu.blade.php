1a435d5a497650f0ec65bd39adfced1f58a8e695
Flazh <al221811691@gmail.com>
2021-03-09 20:50:26 -0600

    Cambios vista menu

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -106,8 +106,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.html" class="brand-link">
-      <img src="{{asset('img/Flazh.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Admin Flazh</span>
+      <img src="{{asset('img/rambo.jpeg')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin NeriMusic</span>
     </a>
 
     <!-- Sidebar -->
@@ -115,10 +115,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset('img/OIP.jpg')}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset('img/rambo.jpeg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Barry Allen</a>
+          <a href="#" class="d-block">Neri</a>
         </div>
       </div>
       <!-- SidebarSearch Form -->
