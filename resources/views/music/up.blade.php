d98b3f7ad75cecd87d79f40b093b25d3caccaf84
Zhamus <al221810713@gmail.com>
2021-03-16 21:03:00 -0600

    vista up

--- resources/views/music/up.blade.php
+++ resources/views/music/up.blade.php
@@ -107,7 +107,7 @@
 			<div class="formulario__grupo" id="grupo__nombrealbum">
 				<label for="nombrealbum" class="formulario__label">Nombre del album:</label>
 				<div class="formulario__grupo-input">
-					<input type="text" class="formulario__input" name="id_album" value="" id="nombrealbum" placeholder="Inserte el nombre">
+					<input type="text" class="formulario__input" name="" value="" id="nombrealbum" placeholder="Inserte el nombre">
 					<i class="formulario__validacion-estado fas fa-times-circle"></i>
 				</div>
 				<p class="formulario__input-error">El nombre tiene que ser de 4 a 16 dígitos y solo puede contener numeros, letras y guion bajo.</p>
